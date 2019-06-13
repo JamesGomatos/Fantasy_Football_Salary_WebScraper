a22c4837a02673cfff7b341a073cfc0e2c4f8402
James Gomatos <James.Gomatos@sas.com>
2019-06-12 23:37:24 -0400

    finished joe's project need to write documentation

--- fantasy_scrape.py
+++ fantasy_scrape.py
@@ -50,17 +50,15 @@ class WebScraper:
     def get_team_player_info(self):
         csv_file = open('fantasy_players.csv', 'w')
         csv_writer = csv.writer(csv_file,  lineterminator='\n')
-        csv_writer.writerow(['Name', 'Position', 'Base Salary',  'Roster Bonus', 'Signing Bonus', 'Cap Hit'])
+        csv_writer.writerow(['Name', 'Position', 'Base Salary',  'Roster Bonus', 'Signing Bonus', 'Option Bonus', 'Workout Bonus', 'Restructure Bonus', 'Incentive Bonus', 'Dead Cap', 'Cap Hit', 'Cap %'])
         for key, value in self.database.items():
             source = requests.get(value).text
             soup = BeautifulSoup(source, 'lxml')
             rows = soup.find_all('tr')
             for entry in rows:
-                #values = entry.find_all('td')
-                #print(values)
                 try: 
                   
-                    # parse the string
+                    # parse the player name
                     player_name = entry.find('td', class_='player')
                     player_name = player_name.a.text
                     player_name = player_name.replace('\t', " ")
@@ -71,33 +69,57 @@ class WebScraper:
                     player_position = entry.find('td', class_='center small')
                     player_position = player_position.span.text
 
+
                     # parse the base salary
                     base_salary = entry.find('td', class_='right xs-hide')
                     base_salary = base_salary.span.text
 
-
-                    all_tds = entry.find_all('td', class_='right xs-hide')
-                   # wtf = all_tds.find('span',{'title':'Signing Bonus'})
-                    #print(wtf)
-                    #roster_bonus = 0
-                    #signing_bonus = 0
-                    for i in all_tds:
-                        austin = i.find('span', {'title':'Roster Bonus'})
-                        jimmy = i.find('span', {'title': 'Signing Bonus'})
-                        if austin != None:
-                            roster_bonus = austin.text
-                        if jimmy != None:
-                            signing_bonus = jimmy.text
-                        #val = i.find('span', {'title':'Signing Bonus'})
-                       # if val != 0:
-                            #signing_bonus = val.text
                     
+                    all_tds = entry.find_all('td', class_='right')
+                    for i in all_tds:
+
+                    
+                        column1 = i.find('span', {'title': 'Roster Bonus'})
+                        column2 = i.find('span', {'title': 'Signing Bonus'})
+                        column3 = i.find('span', {'title': 'Option Bonus'})
+                        column4 = i.find('span', {'title': 'Workout Bonus'})
+                        column5 = i.find('span', {'title': 'Restructure Bonus'})
+                        column6 = i.find('span', {'title': 'Incentive Bonus'})
+                    
+                        if column1 != None:
+                            roster_bonus = column1.text
+                        if column2 != None:
+                            signing_bonus = column2.text
+                        if column3 != None:
+                            option_bonus = column3.text 
+                        if column4 != None:
+                            workout_bonus = column4.text
+                        if column5 != None:
+                            restructure_bonus = column5.text
+                        if column6 != None:
+                            incentive_bonus = column6.text
+                  
+                    
+                   # print(roster_bonus, signing_bonus, option_bonus, workout_bonus, restructure_bonus, incentive_bonus)
+                    # parise the player dead cap 
+                    #player_dead_cap = entry.find_all('td')
+                    player_dead_cap = entry.find('input', class_='d').get('value')
+
+                   # print(player_dead_cap)
+                   # player_dead_cap = player_dead_cap.value
+                   # print(player_dead_cap)
+                    # player_dead_cap = player_dead_cap.span.a
+
                     # parse the player cap hit 
                     player_cap = entry.find('td', class_='right result')
                     player_cap = player_cap.span.text
 
-                    print(player_name, player_position, base_salary, roster_bonus, signing_bonus,  player_cap)
-                    csv_writer.writerow([player_name, player_position, base_salary, roster_bonus, signing_bonus, player_cap])
+                    # parise the cap percentage 
+                    cap_percentage = entry.find('td', class_='center xs-hide')
+                    cap_percentage = cap_percentage.text
+
+                    print(player_name, player_position, base_salary, roster_bonus, signing_bonus, option_bonus, workout_bonus, restructure_bonus, incentive_bonus, player_dead_cap,  player_cap, cap_percentage)
+                    csv_writer.writerow([player_name, player_position, base_salary, roster_bonus, signing_bonus, option_bonus, workout_bonus, restructure_bonus, incentive_bonus, player_dead_cap,  player_cap, cap_percentage])
                 except:
                     print("error")
                     continue
